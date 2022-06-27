dd1e969a4b16b45b84df229e630964dbdcfe942b
Gil Alkobi <86377486+gilalk@users.noreply.github.com>
2022-06-27 16:28:45 +0300

    end of mission

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// function GetYoutubeVideos()
+// {
+//   return({"videosArr":[
+//     {"title": "Relax with Mozart", "youtubeUrl": "https://www.youtube.com/watch?v=mB-QXgtoItA&ab_channel=HALIDONMUSIC", "width": 581 , "height": 327},
+//     {"title": "Relax with Yanni", "youtubeUrl": "https://www.youtube.com/watch?v=Tv3O0uw4_V0&ab_channel=MusicLibrary", "width": 581 , "height": 327},
+//     {"title": "Relax with Beethoven", "youtubeUrl": "https://www.youtube.com/watch?v=jGDtgOHLOZQ&ab_channel=HALIDONMUSIC", "width": 533 , "height": 300},
+//   ]});
+// }
+
+function GetYoutubeVideos() {
+  return ([
+    { title: "Relax with Mozart", youtubeUrl: "https://www.youtube.com/embed/mB-QXgtoItA", width: 581, height: 327 },
+    { title: "Relax with Yanni", youtubeUrl: "https://www.youtube.com/embed/Tv3O0uw4_V0", width: 581, height: 327 },
+    { title: "Relax with Beethoven", youtubeUrl: "https://www.youtube.com/embed/jGDtgOHLOZQ", width: 533, height: 300 },
+  ]);
+}
+
+function YoutubeVideoListComp() {
+  let videos = GetYoutubeVideos();
+  return (
+    <div className='cards'>
+      {videos.map(vid =>
+        <YoutubeVideoComp title={vid.title} url={vid.youtubeUrl} width={vid.width} height={vid.height} />
+      )}
+    </div>);
+}
+
+function YoutubeVideoComp(props) {
+  return (
+    <>
+      <div className='card'>
+        <h1>{props.title}</h1>
+        <iframe width={props.width} height={props.height} src={props.url} title={props.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className='card_iframe'></iframe>
+      </div>
+
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <YoutubeVideoListComp />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
